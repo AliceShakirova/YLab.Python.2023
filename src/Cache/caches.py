81ac256a142018be8112d1ad60249abc9ec08e3b
Alice Shakirova <alice.shakirova.95@gmail.com>
2023-08-08 21:33:37 +0300

    Починка багов кэширования, найденных в процессе рефакторинга тестов

--- src/Cache/caches.py
+++ src/Cache/caches.py
@@ -3,6 +3,7 @@ from os import getenv
 
 import redis
 from pydantic import TypeAdapter
+from redis.client import Pipeline
 
 from src.Cache import cache_settings
 from src.Entities.dish import Dish, DishModel
@@ -20,11 +21,13 @@ else:
 
 
 class MenuCache:
+    all_menus_key = 'all_menus'
+
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_all_menus() -> list[MenuModel]:
+    @classmethod
+    def get_all_menus(cls) -> list[MenuModel]:
         all_menu_ids = MenuCache.get_all_menu_ids()
         if len(all_menu_ids) == 0:
             return []
@@ -35,8 +38,8 @@ class MenuCache:
             result.append(model)
         return result
 
-    @staticmethod
-    def add_menu(menu: Menu) -> MenuModel:
+    @classmethod
+    def add_menu(cls, menu: Menu) -> MenuModel:
         model: MenuModel = MenuModel.model_validate(menu, from_attributes=True)
         redisCache.set(model.id, model.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
         menu_ids = MenuCache.get_all_menu_ids()
@@ -44,45 +47,53 @@ class MenuCache:
         MenuCache.save_all_menu_ids(menu_ids)
         return model
 
-    @staticmethod
-    def get_menu(menu_id: str) -> MenuModel | None:
+    @classmethod
+    def get_menu(cls, menu_id: str) -> MenuModel | None:
         menu_raw = redisCache.get(menu_id)
         if menu_raw is None:
             return None
         return MenuModel.model_validate_json(menu_raw)
 
-    @staticmethod
-    def update_menu(menu: Menu) -> MenuModel:
+    @classmethod
+    def update_menu(cls, menu: Menu) -> MenuModel:
         model: MenuModel = MenuModel.model_validate(menu, from_attributes=True)
         redisCache.set(model.id, model.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
         return model
 
-    @staticmethod
-    def delete_menu(menu_id: str):
-        redisCache.delete(menu_id)
+    @classmethod
+    def delete_menu(cls, menu_id: str):
+        pipe = redisCache.pipeline()
+        pipe.delete(menu_id)
         menu_ids = MenuCache.get_all_menu_ids()
         menu_ids.discard(menu_id)
-        MenuCache.save_all_menu_ids(menu_ids)
+        MenuCache.save_all_menu_ids(menu_ids, pipe)
+        SubmenuCache.delete_all_submenus(menu_id, pipe)
+        pipe.execute()
 
-    @staticmethod
-    def get_all_menu_ids() -> set[str]:
-        menu_ids_raw = redisCache.get('all_menus')
+    @classmethod
+    def get_all_menu_ids(cls) -> set[str]:
+        menu_ids_raw = redisCache.get(cls.all_menus_key)
         if menu_ids_raw is None:
             return set()
         menu_ids = set(json.loads(menu_ids_raw.replace("'", '"')))
         return menu_ids
 
-    @staticmethod
-    def save_all_menu_ids(menu_ids: set[str]):
-        redisCache.set('all_menus', json.dumps(list(menu_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+    @classmethod
+    def save_all_menu_ids(cls, menu_ids: set[str], pipe: Pipeline | None = None):
+        if pipe:
+            pipe.set(cls.all_menus_key, json.dumps(list(menu_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+        else:
+            redisCache.set(cls.all_menus_key, json.dumps(list(menu_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
 
 
 class SubmenuCache:
+    SUBMENUS_KEY_FORMAT = '{0}_submenus'
+
     def __init__(self):
         pass
 
-    @staticmethod
-    def get_submenus(menu_id: str) -> list[SubmenuModel]:
+    @classmethod
+    def get_submenus(cls, menu_id: str) -> list[SubmenuModel]:
         submenu_ids = SubmenuCache.get_submenu_ids(menu_id)
         if not submenu_ids:
             return []
@@ -95,8 +106,8 @@ class SubmenuCache:
             result.append(submenu)
         return result
 
-    @staticmethod
-    def add_submenu(menu_id: str, submenu: Submenu) -> SubmenuModel:
+    @classmethod
+    def add_submenu(cls, menu_id: str, submenu: Submenu) -> SubmenuModel:
         model: SubmenuModel = SubmenuModel.model_validate(submenu, from_attributes=True)
         redisCache.set(model.id, model.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
         submenu_ids = SubmenuCache.get_submenu_ids(menu_id)
@@ -105,56 +116,71 @@ class SubmenuCache:
         SubmenuCache.update_submenu_count(menu_id, 1)
         return model
 
-    @staticmethod
-    def get_submenu(submenu_id: str) -> SubmenuModel | None:
+    @classmethod
+    def get_submenu(cls, submenu_id: str) -> SubmenuModel | None:
         submenu_raw = redisCache.get(submenu_id)
         if submenu_raw is None:
             return None
         return SubmenuModel.model_validate_json(submenu_raw)
 
-    @staticmethod
-    def update_submenu(submenu: Submenu) -> SubmenuModel:
+    @classmethod
+    def update_submenu(cls, submenu: Submenu) -> SubmenuModel:
         model: SubmenuModel = SubmenuModel.model_validate(submenu, from_attributes=True)
         redisCache.set(model.id, model.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
         return model
 
-    @staticmethod
-    def delete_submenu(menu_id: str, submenu_id: str):
+    @classmethod
+    def delete_submenu(cls, menu_id: str, submenu_id: str):
         dishes_count = 0
         dish_ids = DishCache.get_dish_ids(submenu_id)
         if dish_ids is not None:
             dishes_count = len(dish_ids)
-        redisCache.delete(submenu_id)
         submenu_ids = SubmenuCache.get_submenu_ids(menu_id)
         submenu_ids.discard(submenu_id)
         SubmenuCache.save_submenu_ids(menu_id, submenu_ids)
         SubmenuCache.update_submenu_count(menu_id, -1, dishes_count)
+        pipe = redisCache.pipeline()
+        pipe.delete(submenu_id)
+        DishCache.delete_all_dishes(submenu_id, pipe)
+        pipe.delete(cls.SUBMENUS_KEY_FORMAT.format(menu_id))
+        pipe.execute()
 
-    @staticmethod
-    def get_submenu_ids(menu_id: str) -> set[str]:
-        dish_ids_raw = redisCache.get(f'{menu_id}_submenus')
+    @classmethod
+    def delete_all_submenus(cls, menu_id: str, pipe: Pipeline):
+        submenu_ids = cls.get_submenu_ids(menu_id)
+        for submenu_id in submenu_ids:
+            DishCache.delete_all_dishes(submenu_id, pipe)
+            pipe.delete(cls.SUBMENUS_KEY_FORMAT.format(menu_id))
+        pipe.delete(*submenu_ids)
+
+    @classmethod
+    def get_submenu_ids(cls, menu_id: str) -> set[str]:
+        dish_ids_raw = redisCache.get(cls.SUBMENUS_KEY_FORMAT.format(menu_id))
         if dish_ids_raw is None:
             return set()
         dish_ids = set(json.loads(dish_ids_raw.replace("'", '"')))
         return dish_ids
 
-    @staticmethod
-    def save_submenu_ids(menu_id: str, submenu_ids: set[str]):
-        redisCache.set(f'{menu_id}_submenus', json.dumps(list(submenu_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+    @classmethod
+    def save_submenu_ids(cls, menu_id: str, submenu_ids: set[str]):
+        redisCache.set(cls.SUBMENUS_KEY_FORMAT.format(menu_id), json.dumps(
+            list(submenu_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
 
-    @staticmethod
-    def update_submenu_count(menu_id: str, additional_count: int, deleted_dishes_count: int = 0):
+    @classmethod
+    def update_submenu_count(cls, menu_id: str, submenus_additional_count: int, deleted_dishes_count: int = 0):
         menu_raw = redisCache.get(menu_id)
         if menu_raw is None:
             return
         menu: MenuModel = MenuModel.model_validate_json(menu_raw)
-        menu.submenus_count += additional_count
+        menu.submenus_count += submenus_additional_count
         if deleted_dishes_count != 0:
             menu.dishes_count -= deleted_dishes_count
         redisCache.set(menu_id, menu.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
 
 
 class DishCache:
+    DISHES_KEY_FORMAT = '{0}_dishes'
+
     def __init__(self):
         pass
 
@@ -169,7 +195,8 @@ class DishCache:
             result.append(model)
             dish_ids.append(model.id)
             pipe.set(model.id, model.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
-        pipe.set(f'{submenu_id}_dishes', json.dumps(dish_ids), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+        pipe.set(cls.DISHES_KEY_FORMAT.format(submenu_id), json.dumps(
+            dish_ids), ex=cache_settings.CACHE_EXPIRE_SECONDS)
         pipe.execute()
         return result
 
@@ -218,20 +245,21 @@ class DishCache:
         cls.save_dish_ids(submenu_id, dish_ids)
         cls.update_dishes_count(menu_id, submenu_id, -1)
 
-    @staticmethod
-    def get_dish_ids(submenu_id: str) -> set[str]:
-        dish_ids_raw = redisCache.get(f'{submenu_id}_dishes')
+    @classmethod
+    def get_dish_ids(cls, submenu_id: str) -> set[str]:
+        dish_ids_raw = redisCache.get(cls.DISHES_KEY_FORMAT.format(submenu_id))
         if dish_ids_raw is None:
             return set()
         dish_ids = set(json.loads(dish_ids_raw.replace("'", '"')))
         return dish_ids
 
-    @staticmethod
-    def save_dish_ids(submenu_id: str, dish_ids: set[str]):
-        redisCache.set(f'{submenu_id}_dishes', json.dumps(list(dish_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+    @classmethod
+    def save_dish_ids(cls, submenu_id: str, dish_ids: set[str]):
+        redisCache.set(cls.DISHES_KEY_FORMAT.format(submenu_id), json.dumps(
+            list(dish_ids)), ex=cache_settings.CACHE_EXPIRE_SECONDS)
 
-    @staticmethod
-    def update_dishes_count(menu_id, submenu_id: str, additional_dishes_count: int) -> None:
+    @classmethod
+    def update_dishes_count(cls, menu_id, submenu_id: str, additional_dishes_count: int) -> None:
         submenu_raw = redisCache.get(submenu_id)
         if submenu_raw is None:
             return
@@ -244,3 +272,9 @@ class DishCache:
         menu: MenuModel = MenuModel.model_validate_json(menu_raw)
         menu.dishes_count += additional_dishes_count
         redisCache.set(menu_id, menu.model_dump_json(), ex=cache_settings.CACHE_EXPIRE_SECONDS)
+
+    @classmethod
+    def delete_all_dishes(cls, submenu_id: str, pipe: Pipeline):
+        dish_ids = cls.get_dish_ids(submenu_id)
+        pipe.delete(*dish_ids)
+        pipe.delete(cls.DISHES_KEY_FORMAT.format(submenu_id))
